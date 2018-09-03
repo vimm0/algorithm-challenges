8272b285131d9da584c6f521f723dbd71fc396a3
vimm0 <vimmrana0@gmail.com>
2018-09-03 23:26:38 +0545

    add parent child index in heap

--- js/data-structures/heap/Heap.js
+++ js/data-structures/heap/Heap.js
@@ -9,12 +9,52 @@ export default class Heap {
      * @param {Function} [comparatorFunction]
      */
     constructor(comparatorFunction) {
-        // if (new.target === Heap) {
-        //     throw new TypeError('Cannot construct Heap instance directly');
-        // }
+        if (new.target === Heap) {
+            throw new TypeError('Cannot construct Heap instance directly');
+        }
 
         // Array representation of the heap.
         this.heapContainer = [];
         this.compare = new Comparator(comparatorFunction);
+    }
+
+    /**
+     * @param {number} parentIndex
+     * @return {number}
+     */
+    getLeftChildIndex(parentIndex) {
+        return (2 * parentIndex) + 1;
+    }
+
+    /**
+     * @param {number} parentIndex
+     * @return {number}
+     */
+    getRightChildIndex(parentIndex) {
+        return (2 * parentIndex) + 2;
+    }
+
+    /**
+     * @param {number} childIndex
+     * @return {number}
+     */
+    getParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2);
+    }
+
+    /**
+     * @param {number} childIndex
+     * @return {boolean}
+     */
+    hasParent(childIndex) {
+        return this.getParentIndex(childIndex) >= 0;
+    }
+
+    /**
+     * @param {number} parentIndex
+     * @return {boolean}
+     */
+    hasLeftChild(parentIndex) {
+        return this.getLeftChildIndex(parentIndex) < this.heapContainer.length;
     }
 }
