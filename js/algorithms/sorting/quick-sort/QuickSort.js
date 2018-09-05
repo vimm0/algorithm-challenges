27478ecefad3346c818e6a7166ecd59dc28f68ff
vimm0 <vimmrana0@gmail.com>
2018-09-05 21:16:32 +0545

    add sort

--- js/algorithms/sorting/quick-sort/QuickSort.js
+++ js/algorithms/sorting/quick-sort/QuickSort.js
@@ -5,4 +5,14 @@ export default class QuickSort extends Sort {
      * @param {*[]} originalArray
      * @return {*[]}
      */
+    sort(originalArray) {
+        // Clone original array to prevent it from modification.
+        const array = [...originalArray];
+
+        // If array has less than or equal to one elements then it is already sorted.
+        if (array.length <= 1) {
+            return array;
+        }
+
+    }
 }
