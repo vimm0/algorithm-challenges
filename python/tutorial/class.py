f3a76f00f0fe92c670af76de9f85ea2393212bf7
Sandesh <vimmrana0@gmail.com>
2018-09-09 11:05:58 +0545

    add example in class.py

--- python/tutorial/class.py
+++ python/tutorial/class.py
@@ -23,4 +23,19 @@ print(MyClass.__doc__)  # 'A simple example class'
 # Class instantiation uses function notation
 x = MyClass()
 # invoke special method, because initial data initialized there
-x.data  # []
+# x.data  # []
+x.counter = 1
+while x.counter < 10:
+    x.counter = x.counter * 2
+print(x.counter)
+del x.counter
+
+
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+
+x = Complex(3.0, -4.5)
+print(x.r, x.i)  # (3.0, -4.5)
