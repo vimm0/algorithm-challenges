fc1a242a0857221f07ecc4176fa8ed5739ef31d5
Sandesh <vimmrana0@gmail.com>
2018-09-18 09:42:29 +0545

    add write

---
+++ python/example/write.py
@@ -0,0 +1,9 @@
+# from sys module import a member called 'argv'
+from sys import argv
+
+# unpack
+script, filename = argv
+fp = open(filename, "w")
+print("Writing to file %r " % fp)
+fp.write("Hello World")
+fp.close()
