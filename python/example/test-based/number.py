738c42fb21cb2deffd23e59d79e99c71589a746a
vimm0 <vimmrana0@gmail.com>
2018-09-22 21:26:16 +0545

    add float, complexnumber and number operator

--- python/example/test-based/number.py
+++ python/example/test-based/number.py
@@ -45,3 +45,71 @@ def test_booleans():
     # Let's try to cast boolean to string.
     assert str(true_boolean) == "True"
     assert str(false_boolean) == "False"
+
+
+def test_float_numbers():
+    """Float type
+    Float, or "floating point number" is a number, positive or negative,
+    containing one or more decimals.
+    """
+
+    float_number = 7.0
+    # Another way of declaring float is using float() function.
+    float_number_via_function = float(7)
+    float_negative = -35.59
+
+    assert float_number == float_number_via_function
+    assert isinstance(float_number, float)
+    assert isinstance(float_number_via_function, float)
+    assert isinstance(float_negative, float)
+
+    # Float can also be scientific numbers with an "e" to indicate
+    # the power of 10.
+    float_with_small_e = 35e3
+    float_with_big_e = 12E4
+
+    assert float_with_small_e == 35000
+    assert float_with_big_e == 120000
+    assert isinstance(12E4, float)
+    assert isinstance(-87.7e100, float)
+
+
+def test_complex_numbers():
+    """Complex Type"""
+
+    complex_number_1 = 5 + 6j
+    complex_number_2 = 3 - 2j
+
+    assert isinstance(complex_number_1, complex)
+    assert isinstance(complex_number_2, complex)
+    assert complex_number_1 * complex_number_2 == 27 + 8j
+
+
+def test_number_operators():
+    """Basic operations"""
+
+    # Addition.
+    assert 2 + 4 == 6
+
+    # Multiplication.
+    assert 2 * 4 == 8
+
+    # Division always returns a floating point number.
+    assert 12 / 3 == 4.0
+    assert 12 / 5 == 2.4
+    assert 17 / 3 == 5.666666666666667
+
+    # Modulo operator returns the remainder of the division.
+    assert 12 % 3 == 0
+    assert 13 % 3 == 1
+
+    # Floor division discards the fractional part.
+    assert 17 // 3 == 5
+
+    # Raising the number to specific power.
+    assert 5 ** 2 == 25  # 5 squared
+    assert 2 ** 7 == 128  # 2 to the power of 7
+
+    # There is full support for floating point; operators with
+    # mixed type operands convert the integer operand to floating point.
+    assert 4 * 3.75 - 1 == 14.0
